322362b95a55a86ff0e42823c45baad43dfb1ae1
user <asa.witt@mail.mcgill.ca>
2017-12-06 03:43:48 -0500

    minor fix

--- MNIST/MNIST_Preprocessing.py
+++ MNIST/MNIST_Preprocessing.py
@@ -3,9 +3,9 @@ import numpy as np
 import scipy.misc 
 import random
 
-NUM_IMAGES = 2000
-alphas = [0,5]
-output_filename = "datasets/MNIST_uniform_label_noise_"
+NUM_IMAGES = 2500
+alphas = [0,10,20,30,40,50,60,70,80,90,100]
+output_filename = "datasets/ULN" + str(NUM_IMAGES) + "/MNIST_uniform_label_noise_"
 
 label_space = ['0','1','2','3','4','5','6','7','8','9']
 #Load MNIST dataset
@@ -16,9 +16,10 @@ images, labels = mndata.load_training()
 output_x_files = {}
 output_y_files = {}
 
+BINARIZATION_THRESHOLD = 150
 
 def img_to_str(img):
-	img_str = "".join(list(map(lambda x: '1,' if x > 150 else '0,',img)))
+	img_str = "".join(list(map(lambda x: '1,' if x > BINARIZATION_THRESHOLD else '0,',img)))
 	return img_str[:-1]	
 
 def str_to_img(s):
@@ -31,7 +32,8 @@ def main():
 		output_x_files[alpha] = open(output_filename + str(alpha) + "_x", 'w')
 		output_y_files[alpha] = open(output_filename + str(alpha) + "_y", 'w')
 
-	NUM_IMAGES = min(len(NUM_IMAGES),NUM_IMAGES) #in case NUM_IMAGES is too high
+	global NUM_IMAGES
+	NUM_IMAGES = min(len(images),NUM_IMAGES) #in case NUM_IMAGES is too high
 	#For each image we rewrite it to a second file, while throwing in variable number of incorrect labels
 	for image,label in zip(images[0:NUM_IMAGES],labels[0:NUM_IMAGES]):
 		img_str = img_to_str(image)
